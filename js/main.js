50eef8a3da8b4db681320cadb82b45e2a701cb4e
KalininaValeri <kalinina.valeri@gmail.com>
2016-03-30 21:43:43 +0600

    height about

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+  var body = $('body');
+  // the height of the screen
+  body.append('<style>section .about_height{height:' + $( window ).height() + 'px!important;}</style>');
+});
+
+//$(window).height();
