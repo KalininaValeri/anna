1b430a6590e0dd95fd0ddf143b47da750f783eb8
KalininaValeri <kalinina.valeri@gmail.com>
2016-04-01 22:14:50 +0600

    sliding window

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@ $(function () {
   body.append('<style>section .about_height{height:' + $( window ).height() + 'px!important;}</style>');
 });
 
+$( "#toggle" ).click(function() {
+  var text = $( "#text" );
+  if (text.hasClass('active')) {
+    text.slideUp("slow").removeClass('active');
+  } else {
+    text.slideDown("slow").addClass('active');
+  }
+});
+
 //$(window).height();
