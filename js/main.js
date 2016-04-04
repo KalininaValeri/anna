1afbfed298074fca43f17a4935dbcaa233373285
KalininaValeri <kalinina.valeri@gmail.com>
2016-04-04 18:17:51 +0600

    google map

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ $(function () {
 
   $.stellar();
 
+  initMap();
+
   $('.grid').masonry({
     // options
     itemSelector: '.grid-item',
@@ -20,3 +22,39 @@ $( "#toggle" ).click(function() {
     text.slideDown("slow").addClass('active');
   }
 });
+
+  // Create a map object and specify the DOM element for display.
+  function initMap() {
+
+    // Specify features and elements to define styles.
+    var styleArray = [
+      {
+        featureType: "all",
+        stylers: [
+          { saturation: -80 }
+        ]
+      },{
+        featureType: "road.arterial",
+        elementType: "geometry",
+        stylers: [
+          { hue: "#00ffee" },
+          { saturation: 50 }
+        ]
+      },{
+        featureType: "poi.business",
+        elementType: "labels",
+        stylers: [
+          { visibility: "off" }
+        ]
+      }
+    ];
+
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      scrollwheel: false,
+      // Apply the map style array to the map.
+      styles: styleArray,
+      zoom: 8
+    });
+  }
